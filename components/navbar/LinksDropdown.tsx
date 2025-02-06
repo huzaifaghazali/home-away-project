b793b5826776277cf5c8b6cc59ba5f49f0a978ba
huzaifaghazali <huzaifaghazali@gmail.com>
2025-02-06 21:20:02 +0500

    Update links dropdown component

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,3 +1,5 @@
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -21,16 +23,35 @@ function LinksDropdown() {
           <UserIcon />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link href={link.href} className='capitalize w-full'>
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+      <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className='capitalize w-full'>
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,9 +1,20 @@
-
+import { SignOutButton } from '@clerk/nextjs';
+import {} from '../ui/toast';
+import { useToast } from '@/hooks/use-toast';
 
 function SignOutLink() {
+  const { toast } = useToast();
+
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.' });
+  };
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton>
+      <button className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
 
-export default SignOutLink
+export default SignOutLink;
