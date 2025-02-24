db6c26dcf3d4d57ff49ea09d44a06c9be18e1f06
huzaifaghazali <huzaifaghazali@gmail.com>
2025-02-24 19:53:40 +0500

    Add CategoriesInput component

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -3,6 +3,7 @@ import FormContainer from '@/components/form/FormContainer';
 import { createPropertyAction } from '@/utils/actions';
 import { SubmitButton } from '@/components/form/Buttons';
 import PriceInput from '@/components/form/PriceInput';
+import CategoriesInput from '@/components/form/CategoriesInput';
 
 function CreateProperty() {
   return (
@@ -26,10 +27,9 @@ function CreateProperty() {
               label='Tagline (30 limit)'
               defaultValue='Dream Getaway Awaits You Here!'
             />
-            {/* price */}
             <PriceInput />
 
-            {/* categories */}
+            <CategoriesInput />
           </div>
           {/* text area / description */}
           <SubmitButton text='create rental' className='mt-12' />
