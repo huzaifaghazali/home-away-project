5689b29677eedf3d581bad49c88874818226ff0d
huzaifaghazali <huzaifaghazali@gmail.com>
2025-02-25 16:12:37 +0500

    Add CounterInput component

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,7 @@ import CategoriesInput from '@/components/form/CategoriesInput';
 import TextAreaInput from '@/components/form/TextAreaInput';
 import ImageInput from '@/components/form/ImageInput';
 import CountriesInput from '@/components/form/CountriesInput';
+import CounterInput from '@/components/form/CounterInput';
 
 function CreateProperty() {
   return (
@@ -46,7 +47,15 @@ function CreateProperty() {
             <CountriesInput />
             <ImageInput />
           </div>
-          
+
+          <h3 className='text-lg mt-8 mb-4 font-medium'>
+            Accommodation Details
+          </h3>
+          <CounterInput detail='guests' />
+          <CounterInput detail='bedrooms' />
+          <CounterInput detail='beds' />
+          <CounterInput detail='baths' />
+
           <SubmitButton text='create rental' className='mt-12' />
         </FormContainer>
       </div>
