10146a8969ac4cf4560010f46151718456f2d655
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-03 16:41:47 +0500

    Add authentication check to FavoriteToggleButton component

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,12 @@
 import { FaHeart } from 'react-icons/fa';
-import { Button } from '@/components/ui/button';
+import { auth } from '@clerk/nextjs/server';
 
-function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+import { Button } from '@/components/ui/button';
+import { CardSignInButton } from '../form/Buttons';
+
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  const { userId } = await auth();
+  if (!userId) return <CardSignInButton />;
   return (
     <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
       <FaHeart />
