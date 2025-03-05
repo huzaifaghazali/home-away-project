e895906e0892dae9efd3d8d40102c7218e85ca7d
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-05 16:21:35 +0500

    Enhance PropertyDetailsPage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,12 +1,25 @@
 import { redirect } from 'next/navigation';
 import { fetchPropertyDetails } from '@/utils/actions';
+import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  return <div>PropertyDetailsPage</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold '>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
 }
 
 export default PropertyDetailsPage;
