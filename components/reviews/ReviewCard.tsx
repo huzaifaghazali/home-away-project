12242bb779ae88d3551396f0475b6725650fcf4e
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-09 16:06:10 +0500

    Add PropertyReviews component

---
+++ components/reviews/ReviewCard.tsx
@@ -0,0 +1,18 @@
+
+type ReviewCardProps = {
+  reviewInfo: {
+    comment: string;
+    rating: number;
+    name: string;
+    image: string;
+  };
+  children?: React.ReactNode;
+};
+
+function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
+  return (
+    <div>ReviewCard</div>
+  )
+}
+
+export default ReviewCard
