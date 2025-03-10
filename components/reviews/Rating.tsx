0e8e260f21724249629b87381dc90ea392f59254
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-10 16:15:21 +0500

    Add Rating component

--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -1,8 +1,21 @@
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const Rating = ({ rating }: { rating: number }) => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1 <= rating);
   return (
-    <div>Rating</div>
-  )
-}
+    <div className='flex items-center gap-x-1'>
+      {stars.map((isFilled, i) => {
+        const className = `w-3 h-3 ${
+          isFilled ? 'text-primary' : 'text-gray-400'
+        }`;
+        return isFilled ? (
+          <FaStar key={i} className={className} />
+        ) : (
+          <FaRegStar key={i} className={className} />
+        );
+      })}
+    </div>
+  );
+};
 
-export default Rating
+export default Rating;
