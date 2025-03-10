078bf09ebb5fc59ab6d81c85cfe40fc589645457
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-10 19:44:21 +0500

    Add Comment component

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,8 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 const Comment = ({ comment }: { comment: string }) => {
-  return (
-    <div>Comment</div>
-  )
-}
+  const [isExpanded, setIsExpanded] = useState(false);
 
-export default Comment
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+
+  return (
+    <div>
+      <p className='text-sm'>{displayComment}</p>
+      {longComment && (
+        <Button
+          variant='link'
+          className='pl-0 text-muted-foreground'
+          onClick={toggleExpanded}
+        >
+          {isExpanded ? 'Show Less' : 'Show More'}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Comment;
