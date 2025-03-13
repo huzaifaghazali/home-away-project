4ae88a2de62febcb4f2aa227cf22c5ba351ffdec
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-13 19:58:28 +0500

    Add dynamic loading for BookingWrapper in PropertyDetailsPage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -27,6 +27,14 @@ const DynamicMap = dynamic(
   }
 );
 
+const DynamicBookingWrapper = dynamic(
+  () => import('@/components/booking/BookingWrapper'),
+  {
+    ssr: false,
+    loading: () => <Skeleton className='h-[200px] w-full' />,
+  }
+);
+
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect('/');
@@ -64,7 +72,11 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         </div>
         <div className='lg:col-span-4 flex flex-col items-center'>
           {/* Calendar */}
-          <BookingCalendar />
+          <DynamicBookingWrapper
+            propertyId={property.id}
+            price={property.price}
+            bookings={property.bookings}
+          />
         </div>
       </section>
       {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
