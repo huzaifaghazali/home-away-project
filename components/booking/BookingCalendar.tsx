0b03c0e9e6051d881630e6220619332287f2cdad
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-14 21:02:31 +0500

    Enhance BookingCalendar to manage date range state

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,26 +1,32 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { DateRange } from 'react-day-picker';
-import { Calendar } from '@/components/ui/calendar';
 
-import React from 'react';
+import { Calendar } from '@/components/ui/calendar';
+import { useToast } from '@/hooks/use-toast';
+import { useProperty } from '@/utils/store';
+import {
+  generateDisabledDates,
+  generateDateRange,
+  defaultSelected,
+  generateBlockedPeriods,
+} from '@/utils/calendar';
 
 export default function BookingCalendar() {
   const currentDate = new Date();
-  const defaultSelected: DateRange = {
-    from: undefined,
-    to: undefined,
-  };
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
 
+  useEffect(() => {
+    useProperty.setState({ range });
+  }, [range]);
   return (
     <Calendar
-      id='text'
       mode='range'
       defaultMonth={currentDate}
       selected={range}
       onSelect={setRange}
+      className='mb-4'
     />
   );
 }
