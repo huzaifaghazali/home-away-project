eb0ebda86b65e446e7af3d048672afc441e38092
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-23 15:44:32 +0500

    Add fetch Chats data action function

--- utils/actions.ts
+++ utils/actions.ts
@@ -14,6 +14,7 @@ import db from './db';
 import { revalidatePath } from 'next/cache';
 import { uploadImage } from './supabase';
 import { calculateTotals } from './calculateTotals';
+import { formatDate } from './format';
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -659,4 +660,35 @@ export const fetchStats = async () => {
     propertiesCount,
     bookingsCount,
   };
+};
+
+export const fetchChartsData = async () => {
+  await getAdminUser();
+  const date = new Date();
+  date.setMonth(date.getMonth() - 6);
+  const sixMonthsAgo = date;
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  });
+
+  let bookingsPerMonth = bookings.reduce((total, current) => {
+    const date = formatDate(current.createdAt, true);
+    const existingEntry = total.find((entry) => entry.date === date);
+    if (existingEntry) {
+      existingEntry.count += 1;
+    } else {
+      total.push({ date, count: 1 });
+    }
+    return total;
+  }, [] as Array<{ date: string; count: number }>);
+
+  return bookingsPerMonth;
 };
