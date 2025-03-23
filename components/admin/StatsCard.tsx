855e10a46954787503065ed79042cff1cc498d54
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-23 15:35:23 +0500

    Add StatsCard component

--- components/admin/StatsCard.tsx
+++ components/admin/StatsCard.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader } from '@/components/ui/card';
 
 type StatsCardsProps = {
   title: string;
@@ -6,8 +7,13 @@ type StatsCardsProps = {
 
 function StatsCard({ title, value }: StatsCardsProps) {
   return (
-    <div>StatsCard</div>
-  )
+    <Card className='bg-muted'>
+      <CardHeader className='flex flex-row justify-between items-center'>
+        <h3 className='capitalize text-3xl font-bold'>{title}</h3>
+        <span className='text-primary text-5xl font-extrabold'>{value}</span>
+      </CardHeader>
+    </Card>
+  );
 }
 
-export default StatsCard
+export default StatsCard;
