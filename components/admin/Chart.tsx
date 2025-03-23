d09bebe02be77782c72d2f7fba8e4178a516d774
huzaifaghazali <huzaifaghazali@gmail.com>
2025-03-23 15:53:06 +0500

    Add Chart component

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 type ChartPropsType = {
   data: {
@@ -8,8 +19,19 @@ type ChartPropsType = {
 
 function Chart({ data }: ChartPropsType) {
   return (
-    <div>Chart</div>
-  )
+    <section className='mt-24'>
+      <h1 className='text-4xl font-semibold text-center'>Monthly Bookings</h1>
+      <ResponsiveContainer width='100%' height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey='count' fill='#F97215' barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 }
 
-export default Chart
+export default Chart;
